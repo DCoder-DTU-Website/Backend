c1f18ff0dfd1d52a15e355c2ebab6309b49422e7
AdityaTeltia <teltia.aditya22@gmail.com>
2021-04-28 09:10:02 +0530

    Contact Form router

--- routes/contact-router.js
+++ routes/contact-router.js
@@ -1,50 +1,50 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
+  const express = require("express");
+  const nodemailer = require("nodemailer");
 
 
-const router = express.Router();
+  const router = express.Router();
 
-router.post("/send", (req, res) => {
-  const output = `
-        <p>You have a new contact request</p>
-        <h3>Contact Details</h3>
-        <ul>  
-          <li>Name: ${req.body.name}</li>
-          <li>Email: ${req.body.email}</li>
-        </ul>
-        <h3>Message</h3>
-        <p>${req.body.message}</p>
-      `;
+  router.post("/send", (req, res) => {
+    const output = `
+          <p>You have a new contact request</p>
+          <h3>Contact Details</h3>
+          <ul>  
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+          </ul>
+          <h3>Message</h3>
+          <p>${req.body.message}</p>
+        `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "temp24918@gmail.com", // generated ethereal user
-      pass: "temp@999", // generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "temp24918@gmail.com", // generated ethereal user
+        pass: "temp@999", // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+      from: '"Nodemailer Contact" temp24918@gmail.com', // sender address
+      to: "teltia.aditya22@gmail.com", // list of receivers
+      subject: "Node Contact Request", // Subject line
+      text: "Hello world?", // plain text body
+      html: output, // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    });
   });
 
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: '"Nodemailer Contact" temp24918@gmail.com', // sender address
-    to: "teltia.aditya22@gmail.com", // list of receivers
-    subject: "Node Contact Request", // Subject line
-    text: "Hello world?", // plain text body
-    html: output, // html body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  });
-});
-
-module.exports = router;
+  module.exports = router;
