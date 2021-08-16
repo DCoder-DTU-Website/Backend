da1792898fbd5384d9156f17dc2c474136370566
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-08-16 20:23:21 +0530

    New Routes and Controllers for applicants

--- controllers/applicants-ctrl.js
+++ controllers/applicants-ctrl.js
@@ -9,6 +9,35 @@ module.exports.getApplicants = async (req, res) => {
   }
 };
 
+module.exports.getAwaitingApplicants = async (req, res) => {
+  try {
+    const applicants = await UserApplied.find({ interviewCompleted: false });
+    res.status(200).json(applicants);
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+};
+module.exports.getAcceptedApplicants = async (req, res) => {
+  try {
+    const applicants = await UserApplied.find({ isAccepted: true });
+    res.status(200).json(applicants);
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+};
+
+module.exports.getRejectedApplicants = async (req, res) => {
+  try {
+    const applicants = await UserApplied.find({
+      isAccepted: false,
+      interviewCompleted: true,
+    });
+    res.status(200).json(applicants);
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+};
+
 module.exports.createApplicant = async (req, res) => {
   const new_applicant = new UserApplied(req.body);
   try {
@@ -17,6 +46,20 @@ module.exports.createApplicant = async (req, res) => {
     });
   } catch (error) {
     res.status(400).json(error);
+  }
+};
+
+module.exports.updateApplicant = async (req, res) => {
+  const id = req.params.id;
+  const update_applicant = new UserApplied(req.body);
+  try {
+    await UserApplied.findOneAndUpdate({ _id: id }, update_applicant).then(
+      () => {
+        res.status(202).json({ _id: id });
+      }
+    );
+  } catch (error) {
+    res.status(401).json({ message: error.message });
   }
 };
 
--- routes/applicant-router.js
+++ routes/applicant-router.js
@@ -5,7 +5,19 @@ const authCtrl = require("../controllers/auth-ctrl");
 const router = express.Router();
 
 router.get("/applicants/all", applicantCtrl.getApplicants);
+router.get("/applicants/awaiting", applicantCtrl.getAwaitingApplicants);
+router.get("/applicants/accepted", applicantCtrl.getAcceptedApplicants);
+router.get("/applicants/rejected", applicantCtrl.getRejectedApplicants);
 router.post("/applicants", applicantCtrl.createApplicant);
-router.delete("/applicants/:id", applicantCtrl.removeApplicant);
+router.put(
+  "/applicants/:id",
+  authCtrl.authenticateTokenAdmin,
+  applicantCtrl.updateApplicant
+);
+router.delete(
+  "/applicants/:id",
+  authCtrl.authenticateTokenAdmin,
+  applicantCtrl.removeApplicant
+);
 
 module.exports = router;
