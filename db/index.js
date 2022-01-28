6787af0f1b7e4491bdeae1c20dde67b1be98f963
Naman Malhotra <namalhotra333@gmail.com>
2022-01-29 00:00:13 +0530

    fixed bugs

--- db/index.js
+++ db/index.js
@@ -5,7 +5,8 @@ mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(() => {
+  })
+  .then(() => {
     console.log("Database Connected Successfully");
   })
   .catch((e) => {
